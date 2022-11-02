da86862d11bb3fab25a54f9b1ade68b9f4ed2913
abdc06 <abdc2806@gmail.com>
2022-11-02 23:06:49 +0900

    servlet, handler 등록

---
+++ servlet-app/src/main/java/me/abdc/HelloServlet.java
@@ -0,0 +1,34 @@
+package me.abdc;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("init");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("doGet");
+
+        resp.getWriter().write("<html>");
+        resp.getWriter().write("<head>");
+        resp.getWriter().write("<title>Home</title>");
+        resp.getWriter().write("</head>");
+        resp.getWriter().write("<body>");
+        resp.getWriter().write("<h1>Hello Servlet!!</h1>");
+        resp.getWriter().write("</body>");
+        resp.getWriter().write("</html>");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("destroy");
+    }
+}
